87aea22f9429001a61f002ee70096920e5113c3c
engineking <engineking2023@gmail.com>
2025-08-15 15:40:46 +0200

    test call and anim

--- main.js
+++ main.js
@@ -2,15 +2,6 @@
 
 
 
-let theSong = document.getElementById("song");
-let logo = document.getElementById("logo");
-let myDocument = document.documentElement;
-let btn =document.getElementById("btn");
-let button1 =document.querySelector(".button1");
-let close =document.querySelector(".close");
-let aboutUs = document.querySelector(".aboutUs");
-let isShowAboutUs = true;
-let a =document.querySelector("a");
 
 
 let slideIndex = 0;
@@ -19,18 +10,13 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 500); // Change image every 2 seconds
 }
 
 function showAboutUs(){
@@ -49,76 +35,3 @@ function closeAboutUs(){
 }
 
 
-button1.addEventListener("click",()=>{
-    if(button1.textContent == "About us"){
-
-        if(myDocument.requestFullscreen){
-            myDocument.requestFullscreen();
-        }
-       else if(myDocument.msRequestFullscreen){
-            myDocument.msRequestFullscreen();
-        }
-        else if(myDocument.mozRequestFullscreen){
-            myDocument.mozRequestFullscreen();
-        }
-        
-        else if(myDocument.webkitRequestFullscreen){
-            myDocument.webkitRequestFullscreen();
-        }
-
-
-
-        
-        button1.style.display="none";
-        button2.style.display="block";
-        button3.style.display="block";
-        button4.style.display="block";
-        button5.style.display="block";
-        button6.style.display="block";
-    }
-    
-            
-    
-    
-})
-
-close.addEventListener("click",()=>{
-    if(close.textContent == "Close"){if(document.exitFullscreen){
-                document.exitFullscreen();
-            }
-            else if(document.msexitFullscreen){
-                document.msexitFullscreen();
-            }
-            else if(document.mozexitFullscreen){
-                document.mozexitFullscreen();
-            }
-            else if(document.webkitexitFullscreen){
-                document.webkitexitFullscreen();
-            }
-
-
-
-
-           
-            button1.style.display="block";
-            button2.style.display="none";
-            button3.style.display="none";
-            button4.style.display="none";
-            button5.style.display="none";
-            button6.style.display="none";
-        }
-
-    })
-logo.onclick= function(){
-    
-    if(theSong.paused){
-        
-        theSong.play();
-        logo.src = "images/POUS.png"
-    }
-    else{
-        theSong.pause();
-        logo.src= "images/PLY.png";
-
-    }
-}
